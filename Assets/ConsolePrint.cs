2acab684781762b6f77c2e9bd5ab4a2aea94ab70
VKelvinWang <62001746+VKelvinWang@users.noreply.github.com>
2020-08-19 15:49:08 +0700

    80 percent complete

---
+++ Assets/ConsolePrint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    public GameObject Obj1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Debug.Log(Obj1 + " : " + i);
+        }
+    }
+}
---
+++ Assets/LoadAssets.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssets : MonoBehaviour
+{
+
+    public GameObject redObj;
+    [SerializeField] public GameObject blueObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
